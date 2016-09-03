b0c64e5cc52981ddd2a57d0e1910d56d5b5008ea
syzer3@gmail.com <syzer3@gmail.com>
2016-09-03 12:00:24 +0200

    es6

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var app = express();
+let express = require('express')
+let app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', function (request, response) {
+    response.render('pages/index')
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.post('/', (req, res) => {
+    response.json({
+        i: 'am alive'
+    })
+})
+
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'))
+})
+
+
 
 
