e342444996c9284a7c02125288f014f9a464ed0d
syzer3@gmail.com <syzer3@gmail.com>
2016-09-06 20:00:53 +0200

    fixed test

--- test/test.js
+++ test/test.js
@@ -1,50 +1,30 @@
-import request from 'supertest'
+import request from 'supertest-as-promised'
 import test from 'ava'
 import {app} from '../index'
 
-const data = [{1: 'ok'}, 'plain-text']
+const data = {1: 'ok'}
 
-test('adding keys', t => {
-    request(app)
-        .post('/')
-        .set('Accept', 'application/json')
-        .send(data[0])
-        .expect('Content-Type', /json/)
-        .end(err => {
-            if (err) {
-                return t.fail(err)
-            }
-            return t.pass()
-        })
-})
+test('adding keys', () => request(app)
+    .post('/')
+    .set('Accept', 'application/json')
+    .send(data)
+    .expect(200)
+    .expect('transfer-encoding', /chunked/)
+)
 
-test('reading stream', t => {
-    request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .end(err => {
-            if (err) {
-                return t.fail(err)
-            }
-            return t.pass()
-        })
-})
+test('reading stream', (t) => request(app)
+    .get('/')
+    .set('Accept', 'application/json')
+    .expect(200)
+    .then(d => t.regex(d.text, /1/))
+)
 
-test('querying database', t => {
-    const query = [
+test('querying database', t => request(app)
+    .get(`/?q=${[
         JSON.stringify({gt: '1473017287456'}),
         'limit=1'
-    ].join('&')
-
-    request(app)
-        .get(`/?q=${query}`)
-        .set('Accept', 'application/json')
-        .set('Accept-Encoding', 'gzip,deflate')
-        .end((err, res) => {
-            if (err) {
-                return t.fail(err)
-            }
-            t.truthy(res.text)
-            return t.pass()
-        })
-})
+    ].join('&')}`)
+    .set('Accept', 'application/json')
+    .set('Accept-Encoding', 'gzip,deflate')
+    .then(d => t.regex(d.text, /"ok"/))
+)
