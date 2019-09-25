6680382541e144f76e40323c383e207d40ded383
lukasz gintowt <lukasz.gintowt@rsnag.ch>
2019-09-25 15:53:03 +0200

    Add ability to receive messages via UDP/socket (it's way faster then TCP but you cannot be sure if write was successful). Docs about heroku should mention their container setup

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const path = require('path')
+const dgram = require('dgram')
 const express = require('express')
 const db = require('level')(path.resolve(__dirname, 'data'))
 const compression = require('compression')
 const bodyParser = require('body-parser')
 
-let app = express()
+const udpPort = process.env.UDP_PORT || 5001
+const tcpPort = process.env.PORT || 5000
+const socket = dgram.createSocket('udp4')
+
+const app = express()
 app.use(compression())
 app.use(bodyParser.json())
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (tcpPort))
 
 app.get('/', (req, res) => db.createReadStream(
   Object.assign(
@@ -22,19 +27,31 @@ app.get('/', (req, res) => db.createReadStream(
 )
 
 app.post('/', (req, res) =>
-  db.put(Date.now(), req.body, {encoding: 'json'}, err => {
-    if (err) {
-      return res.status(500).end()
-    }
-
-    return res.status(200).end()
-  })
+  db.put(Date.now(), req.body, {encoding: 'json'})
+    .then(() => res.status(200).end())
+    .catch(() => res.status(500).end())
 )
 
+socket.on('listening', () =>
+  console.log(`UDP server is running on port`, socket.address().port)
+)
+
+socket.on('error', err =>
+  console.error(`UDP error: ${err.stack}`)
+)
+
+socket.on('message', msg =>
+  db.put(Date.now(), JSON.parse(msg.toString()), {encoding: 'json'})
+)
+
+socket.bind(udpPort)
+
 app.listen(app.get('port'), () =>
-  console.log('Node app is running on port', app.get('port'))
+  console.log('TCP server is running on port', app.get('port'))
 )
 
 module.exports = {
-  app
+  app,
+  socket,
+  udpPort
 }
